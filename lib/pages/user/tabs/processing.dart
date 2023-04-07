b3e4dd06205815768f9f289cb8acd8c690d8a3f0
Jodelmar Beltran <jodelbeltran10@gmail.com>
2023-04-07 11:42:31 +0800

    display all bookings

--- lib/pages/user/tabs/processing.dart
+++ lib/pages/user/tabs/processing.dart
@@ -1,23 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:freight/components/bookingItem.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:freight/services/dataService.dart';
 
-class Processing extends StatelessWidget {
+class Processing extends StatefulWidget {
   const Processing({super.key});
 
   @override
+  State<Processing> createState() => _ProcessingState();
+}
+
+class _ProcessingState extends State<Processing> {
+  final Stream<QuerySnapshot> collectionReference = DataService.readBookings();
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: ListView(
-        children: [
-          BookingItem(
-            title: 'Item 1',
-          ),
-          BookingItem(
-            title: 'Item 2',
-          ),
-        ],
-      ),
+    return StreamBuilder(
+      stream: collectionReference,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasData) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (BuildContext context, int index) {
+                return BookingItem(
+                  title: snapshot.data!.docs[index]['origin'],
+                );
+              },
+            );
+          }
+        }
+        return Container();
+      },
     );
   }
 }
