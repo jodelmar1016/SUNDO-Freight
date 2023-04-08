faba0ece7d615769069f0d425fdc44aae998ae73
Jodelmar Beltran <jodelbeltran10@gmail.com>
2023-04-08 22:17:42 +0800

    created addBooking function

--- lib/services/dataService.dart
+++ lib/services/dataService.dart
@@ -1,14 +1,69 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freight/models/response.dart';
+import 'package:freight/models/bookings.dart';
 
 final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 final CollectionReference _collection = _firestore.collection('bookings');
 
 class DataService {
-  static Response addBookings() {
-    Response response = Response();
-    response.code = 500;
-    response.message = 'FAILED';
+  // static Response addBookings() {
+  //   Response response = Response();
+  //   response.code = 500;
+  //   response.message = 'FAILED';
+  //   return response;
+  // }
+
+  // static test(Booking newBooking) {
+  //   print(newBooking.origin);
+  //   print(newBooking.destination);
+  //   print(newBooking.type_of_cargo);
+  //   print(newBooking.weight);
+  //   print(newBooking.length);
+  //   print(newBooking.width);
+  //   print(newBooking.height);
+  //   print(newBooking.cost);
+  //   print(newBooking.senderName);
+  //   print(newBooking.senderContactNo);
+  //   print(newBooking.receiverName);
+  //   print(newBooking.receiverContactNo);
+  // }
+
+  static Future<Response> addBooking(Booking newBooking) async {
+    GeoPoint origin = GeoPoint(
+      newBooking.origin.latitude,
+      newBooking.origin.longitude,
+    );
+    GeoPoint destination = GeoPoint(
+      newBooking.destination.latitude,
+      newBooking.destination.longitude,
+    );
+
+    Response response = new Response();
+    DocumentReference documentReferencer = _collection.doc();
+
+    Map<String, dynamic> data = <String, dynamic>{
+      'origin': origin,
+      'destination': destination,
+      'type_of_cargo': newBooking.type_of_cargo,
+      'weight': newBooking.weight,
+      'length': newBooking.length,
+      'width': newBooking.width,
+      'height': newBooking.height,
+      'cost': newBooking.cost,
+      'sender_name': newBooking.senderName,
+      'sender_contact_no': newBooking.senderContactNo,
+      'receiver_name': newBooking.receiverName,
+      'receiver_contact_no': newBooking.receiverContactNo,
+    };
+
+    var result = await documentReferencer.set(data).whenComplete(() {
+      response.code = 200;
+      response.message = 'Sucessfully added to the database';
+    }).catchError((e) {
+      response.code = 500;
+      response.message = e;
+    });
+
     return response;
   }
 
