cce575b9ef4d691a6309a7c626f9936e995a357d
Jodelmar Beltran <jodelbeltran10@gmail.com>
2023-04-08 22:15:33 +0800

    added subtitle on Booking item

--- lib/components/bookingItem.dart
+++ lib/components/bookingItem.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 
 class BookingItem extends StatelessWidget {
   final String title;
+  final String subtitle;
   const BookingItem({
     super.key,
     required this.title,
+    required this.subtitle,
   });
 
   @override
@@ -13,6 +15,7 @@ class BookingItem extends StatelessWidget {
       elevation: 2,
       child: ListTile(
         title: Text(title),
+        subtitle: Text(subtitle),
         leading: Icon(Icons.location_pin),
         trailing: Icon(Icons.more_vert),
       ),
