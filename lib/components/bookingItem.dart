deabd0fde6e98df0c189caa6ae2c5e2cd24436bd
Jodelmar Beltran <jodelbeltran10@gmail.com>
2023-04-13 03:27:13 +0800

    improve booking item

--- lib/components/bookingItem.dart
+++ lib/components/bookingItem.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:freight/functions/gmap.dart';
+import 'package:freight/pages/user/tracking.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class BookingItem extends StatelessWidget {
-  final String title;
-  final String subtitle;
+  final String trackingNumber;
+  final String origin;
+  final String destination;
+  final LatLng originLatLng;
+  final LatLng destinationLatLng;
+  final double totalCost;
   const BookingItem({
     super.key,
-    required this.title,
-    required this.subtitle,
+    required this.trackingNumber,
+    required this.origin,
+    required this.originLatLng,
+    required this.destination,
+    required this.destinationLatLng,
+    required this.totalCost,
   });
 
   @override
@@ -14,10 +25,30 @@ class BookingItem extends StatelessWidget {
     return Card(
       elevation: 2,
       child: ListTile(
-        title: Text(title),
-        subtitle: Text(subtitle),
-        leading: Icon(Icons.location_pin),
-        trailing: Icon(Icons.more_vert),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (contect) =>
+                  Tracking(points: [originLatLng, destinationLatLng]),
+            ),
+          );
+        },
+        title: Text('Tracking No.: $trackingNumber'),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Text('Sender: $sender'),
+            // Text('Receiver: $receiver'),
+            Text('Origin: $origin'),
+            Text('Destination: $destination'),
+            Divider(color: Colors.teal),
+            Text(
+              'Total Cost: $totalCost',
+              style: TextStyle(color: Colors.teal, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
       ),
     );
   }
