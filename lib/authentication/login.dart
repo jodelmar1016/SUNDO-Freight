a610e9659d595051c2356ba047adeda0fae71e4c
Jodelmar Beltran <jodelbeltran10@gmail.com>
2023-04-19 20:19:04 +0800

    working login

--- lib/authentication/login.dart
+++ lib/authentication/login.dart
@@ -1,107 +1,176 @@
 import 'package:flutter/material.dart';
+import 'package:freight/models/response.dart';
 import 'package:freight/pages/wrapper.dart';
 import 'package:freight/router.dart';
 import 'dart:async';
+import 'package:freight/services/auth.dart';
 
 class Login extends StatefulWidget {
-  const Login({super.key});
-
   @override
   State<Login> createState() => _LoginState();
 }
 
 class _LoginState extends State<Login> {
   bool passwordObscured = true;
-
   bool _isLoading = false;
 
-  void _handleLogin() {
-    setState(() {
-      _isLoading = true;
-    });
+  final _email = TextEditingController();
+  final _password = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final AuthService _auth = AuthService();
 
-    // Simulate a delay before hiding the loading indicator
-    Timer(Duration(seconds: 2), () {
-      setState(() {
-        _isLoading = false;
-      });
-      Navigator.pushReplacement(
-        context,
-        MyPageRouter(page: Wrapper()),
-      );
-    });
+  @override
+  void dispose() {
+    _email.dispose();
+    _password.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    // EMAIL
+    final email = TextFormField(
+      controller: _email,
+      validator: (value) {
+        if (value != null) {
+          if (value.contains('@') && value.endsWith('.com')) {
+            return null;
+          }
+          return 'Enter a Valid Email Address';
+        }
+      },
+      decoration: InputDecoration(
+        labelText: "Email",
+        border: OutlineInputBorder(),
+        prefixIcon: Icon(Icons.email),
+      ),
+    );
+    // PASSWORD
+    final password = TextFormField(
+      controller: _password,
+      obscureText: passwordObscured,
+      validator: (value) {
+        if (value == null || value.trim().isEmpty) {
+          return 'This field is required';
+        }
+        if (value.trim().length < 8) {
+          return 'Password must be at least 8 characters in length';
+        }
+        return null;
+      },
+      decoration: InputDecoration(
+        labelText: "Password",
+        border: OutlineInputBorder(),
+        prefixIcon: Icon(Icons.lock),
+        suffixIcon: IconButton(
+          onPressed: () {
+            setState(() {
+              passwordObscured = !passwordObscured;
+            });
+          },
+          icon: Icon(
+            passwordObscured ? Icons.visibility_off : Icons.visibility,
+          ),
+        ),
+      ),
+    );
+    // LOGIN GUEST BUTTON
+    final guest = ElevatedButton(
+      onPressed: () async {
+        // dynamic result = await _auth.signInAnonymous();
+
+        // if (result.uid == null) {
+        //   showDialog(
+        //       context: context,
+        //       builder: (context) {
+        //         return AlertDialog(
+        //           content: Text(result.code),
+        //         );
+        //       });
+        // }
+      },
+      child: Text('GUEST'),
+    );
+    // LOGIN BUTTON
+    final loginButton = ElevatedButton(
+      onPressed: () async {
+        if (_formKey.currentState!.validate()) {
+          setState(() {
+            _isLoading = true;
+          });
+          Response result = await _auth.signInEmailPassword(
+              _email.text.trim(), _password.text.trim());
+          setState(() {
+            _isLoading = false;
+          });
+          if (result.code == 500) {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  content: Text(result.message.toString()),
+                );
+              },
+            );
+          } else {
+            Navigator.pushReplacement(
+              context,
+              MyPageRouter(
+                page: Wrapper(),
+                begin: Offset(0.0, 1.0),
+                duration: 300,
+              ),
+            );
+          }
+        }
+      },
+      child: Text('LOG IN'),
+    );
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image(
-                    image: AssetImage('assets/logo-text.png'),
-                    color: Colors.teal),
-                SizedBox(height: 70),
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: "Email",
-                    labelText: "Email",
-                    border: OutlineInputBorder(),
+          Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image(
+                      image: AssetImage('assets/logo-text.png'),
+                      color: Colors.teal),
+                  SizedBox(height: 70),
+                  email,
+                  SizedBox(height: 30),
+                  password,
+                  SizedBox(height: 30),
+                  loginButton,
+                  // SizedBox(height: 30),
+                  // guest,
+                  SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Does not have an account yet?'),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text('SIGN UP'),
+                      ),
+                    ],
                   ),
-                ),
-                SizedBox(height: 30),
-                TextFormField(
-                  obscureText: passwordObscured,
-                  decoration: InputDecoration(
-                      hintText: "Password",
-                      labelText: "Password",
-                      border: OutlineInputBorder(),
-                      suffixIcon: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            passwordObscured = !passwordObscured;
-                          });
-                        },
-                        icon: Icon(
-                          passwordObscured
-                              ? Icons.visibility_off
-                              : Icons.visibility,
-                        ),
-                      )),
-                ),
-                SizedBox(height: 30),
-                ElevatedButton(
-                    onPressed: () {
-                      _handleLogin();
-                      // Navigator.pushReplacement(
-                      //   context,
-                      //   MaterialPageRoute(
-                      //     builder: (context) => Wrapper(),
-                      //   ),
-                      // );
-                    },
-                    child: Text('LOG IN')),
-                SizedBox(height: 30),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('Does not have an account yet?'),
-                    TextButton(onPressed: () {}, child: Text('SIGN UP')),
-                  ],
-                ),
-              ],
+                ],
+              ),
             ),
           ),
           if (_isLoading)
             Container(
               color: Colors.black.withOpacity(0.5),
               child: Center(
-                child: CircularProgressIndicator(),
+                child: CircularProgressIndicator(
+                  backgroundColor: Colors.grey[200],
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.teal),
+                ),
               ),
             ),
         ],
