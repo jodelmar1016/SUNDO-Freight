fada53e7a6500fe0704654ba0da39116104ba68e
Jodelmar Beltran <jodelbeltran10@gmail.com>
2023-05-14 02:43:10 +0800

    modify tabs

--- lib/pages/tabs/cancelled.dart
+++ lib/pages/tabs/cancelled.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:freight/components/bookingItem.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:freight/services/dataService.dart';
+import 'package:freight/models/bookings.dart';
 
-class Cancelled extends StatelessWidget {
+class Cancelled extends StatefulWidget {
   const Cancelled({super.key});
 
   @override
+  State<Cancelled> createState() => _CancelledState();
+}
+
+class _CancelledState extends State<Cancelled> {
+  final Stream<QuerySnapshot> collectionReference =
+      DataService.readBookings('cancelled');
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: ListView(
-        children: [],
-      ),
+    return StreamBuilder(
+      stream: collectionReference,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasData) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (BuildContext context, int index) {
+                Booking data = DataService.mapData(snapshot.data!.docs[index]);
+                return BookingItem(data: data);
+              },
+            );
+          }
+        }
+        return Container();
+      },
     );
   }
 }
--- lib/pages/tabs/processing.dart
+++ lib/pages/tabs/processing.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freight/services/dataService.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:freight/functions/gmap.dart';
+import 'package:freight/models/bookings.dart';
 
 class Processing extends StatefulWidget {
   const Processing({super.key});
@@ -14,7 +15,7 @@ class Processing extends StatefulWidget {
 
 class _ProcessingState extends State<Processing> {
   final Stream<QuerySnapshot> collectionReference =
-      DataService.readBookings('processing');
+      DataService.getProcessingAndForPickUp();
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
@@ -29,19 +30,8 @@ class _ProcessingState extends State<Processing> {
             return ListView.builder(
               itemCount: snapshot.data!.docs.length,
               itemBuilder: (BuildContext context, int index) {
-                GeoPoint origin = snapshot.data!.docs[index]['origin'];
-                GeoPoint destination =
-                    snapshot.data!.docs[index]['destination'];
-
-                return BookingItem(
-                  trackingNumber: 'SDFKE-EORK-TKRPK',
-                  origin: snapshot.data!.docs[index]['origin_name'],
-                  destination: snapshot.data!.docs[index]['destination_name'],
-                  originLatLng: LatLng(origin.latitude, origin.longitude),
-                  destinationLatLng:
-                      LatLng(destination.latitude, destination.longitude),
-                  totalCost: snapshot.data!.docs[index]['cost'],
-                );
+                Booking data = DataService.mapData(snapshot.data!.docs[index]);
+                return BookingItem(data: data);
               },
             );
           }
