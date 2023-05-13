512ebc6ea05d6f66e79655ec9aa55d484048e21f
Jodelmar Beltran <jodelbeltran10@gmail.com>
2023-05-14 02:42:05 +0800

    separate functionfor processing and pickup

--- lib/services/dataService.dart
+++ lib/services/dataService.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:freight/models/response.dart';
 import 'package:freight/models/bookings.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 final CollectionReference _collection = _firestore.collection('bookings');
@@ -30,6 +31,66 @@ class DataService {
 
     Query query = bookingItemCollection.where('status', isEqualTo: status);
     return query.snapshots();
+  }
+
+  static Stream<QuerySnapshot> getProcessingAndForPickUp() {
+    CollectionReference bookingItemCollection = _collection;
+    bookingItemCollection.get();
+
+    Query query = bookingItemCollection
+        .where('status', whereIn: ['processing', 'for pick up']);
+    return query.snapshots();
+  }
+
+  static Future<bool> calcelBooking(docId) async {
+    try {
+      _collection.doc(docId).update({
+        'status': 'cancelled',
+        'waiting': false,
+      });
+      return true;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+
+  static Booking mapData(DocumentSnapshot snapshot) {
+    Booking data = new Booking();
+
+    // CONVERT GEOPOINT TO LATLNG
+    GeoPoint geoOrigin = snapshot['origin'];
+    LatLng origin = LatLng(geoOrigin.latitude, geoOrigin.longitude);
+    GeoPoint geoDestination = snapshot['destination'];
+    LatLng destination =
+        LatLng(geoDestination.latitude, geoDestination.longitude);
+
+    // ADD DATA
+    data.docId = snapshot.id;
+    data.origin = origin;
+    data.destination = destination;
+    data.originName = snapshot['origin_name'];
+    data.destinationName = snapshot['destination_name'];
+    data.type_of_cargo = snapshot['type_of_cargo'];
+    data.type_of_vehicle = snapshot['type_of_vehicle'];
+    data.weight = snapshot['weight'];
+    data.length = snapshot['length'];
+    data.width = snapshot['width'];
+    data.height = snapshot['height'];
+    data.distance = snapshot['distance'];
+    data.senderName = snapshot['sender_name'];
+    data.senderContactNo = snapshot['sender_contact_no'];
+    data.receiverName = snapshot['receiver_name'];
+    data.receiverContactNo = snapshot['receiver_contact_no'];
+    data.cost = snapshot['cost'];
+    data.costWithFee = snapshot['cost_with_fee'];
+    data.paymentMethod = snapshot['payment_method'];
+    data.status = snapshot['status'];
+    data.trackingNo = snapshot['tracking_number'];
+
+    data.display();
+
+    return data;
   }
   // For future purposes
   // static printDocument() async {
