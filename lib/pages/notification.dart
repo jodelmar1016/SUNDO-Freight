e9da2ffaf74fcf8c57935fc95df3384c80fd0517
Jodelmar Beltran <jodelbeltran10@gmail.com>
2023-05-21 18:15:36 +0800

    Display notification

--- lib/pages/notification.dart
+++ lib/pages/notification.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:freight/services/dataService.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class NotificationPage extends StatelessWidget {
+class NotificationPage extends StatefulWidget {
   const NotificationPage({super.key});
 
   @override
+  State<NotificationPage> createState() => _NotificationPageState();
+}
+
+class _NotificationPageState extends State<NotificationPage> {
+  bool _isLoading = true;
+  String userId = '';
+  Stream<QuerySnapshot>? notification;
+
+  Future<void> loadData() async {
+    await DataService.getUserId();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column();
+    return StreamBuilder(
+      stream: DataService.getNotification(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasData) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text('${snapshot.data!.docs[index]['title']}'),
+                  subtitle: Text('${snapshot.data!.docs[index]['message']}'),
+                );
+              },
+            );
+          }
+        }
+        return Container();
+      },
+    );
   }
 }
